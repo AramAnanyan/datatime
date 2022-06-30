0c1cfc4457b40691133aaac465c7b4e66b018cc0
user <user@WIN-J3P3EM0J47H>
2022-06-30 13:28:14 +0400

    Add project files.

---
+++ basic/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace basic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            DateTime a = DateTime.Now;            //30 / 06 / 2022 13:21:20
+            Console.WriteLine(a);
+
+            DateTime b=new DateTime(2022, 06, 30, 13, 21, 20);
+            Console.WriteLine(b);
+        }
+    }
+}
